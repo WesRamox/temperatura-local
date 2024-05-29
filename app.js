64d94c94e9b88e933833f6c05a7ff5420b3bb6d3
WesRamox <wesleyramox258@gmail.com>
2024-05-29 19:22:06 -0300

    Updates clean code

--- app.js
+++ app.js
@@ -1,34 +1,31 @@
 async function getWeather() {
-    let stateSelected = document.getElementById("stateSelected")
-    const requestApp = await fetch(`https://api.weatherapi.com/v1/current.json?key=1d34254f1395482192e124938240205&q=${stateSelected.value}&aqi=no`)
-    requestApp.json().then((res) => {
-        return res
-    }).then((data) => {
-        let listDetails = document.getElementById("list-details")
-        listDetails.innerHTML = 
-        `
-        <div id="details-response">
-            <h2 class="state-name">${data.location.name}</h2>
-            <img src="${data.current.condition.icon}" alt="clima" id="img-temp">
-            <h1>${data.current.temp_c}°C</h1>
-            <div class="details">
-                <div id="humidityBox">
-                    <p>Humidity</p>
-                    <p class="subtitle">${data.current.humidity}%</p>
-                </div>
-                <div id="windBox">
-                    <p>Wind speed</p>
-                    <p class="subtitle">${data.current.wind_kph}km/h</p>
-                </div>
-            </div>
-        </div>`
+    let stateSelected = document.getElementById("stateSelected").value
+    const requestApp = await fetch(`https://api.weatherapi.com/v1/current.json?key=1d34254f1395482192e124938240205&q=${stateSelected}&aqi=no`)
+    const data = await requestApp.json()
 
-        stateSelected.value = ""
-    }).catch((err) => {
-        return err
-    })
+    let listDetails = document.getElementById("list-details")
+    listDetails.innerHTML = 
+    `
+    <div id="details-response">
+        <h2 class="state-name">${data.location.name}</h2>
+        <img src="${data.current.condition.icon}" alt="clima" id="img-temp">
+        <h1>${data.current.temp_c}°C</h1>
+        <div class="details">
+            <div id="humidityBox">
+                <p>Humidity</p>
+                <p class="subtitle">${data.current.humidity}%</p>
+            </div>
+            <div id="windBox">
+                <p>Wind speed</p>
+                <p class="subtitle">${data.current.wind_kph}km/h</p>
+            </div>
+        </div>
+    </div>`
+  
+    stateSelected = ""
 }
 
+document.getElementById("sendButton").addEventListener("click", getWeather)
 document.getElementById("stateSelected").addEventListener('keypress', function(event) {
     if (event.key === 'Enter') {
       event.preventDefault();
